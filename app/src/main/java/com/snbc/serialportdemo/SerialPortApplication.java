28acfa3ad0eba769d080982f0ecdfae226466446
zhougaoxiong.newbeiyang <zhougaociongcall@163.com>
2019-07-10 17:14:30 +0800

    Initial commit

---
+++ app/src/main/java/com/snbc/serialportdemo/SerialPortApplication.java
@@ -0,0 +1,49 @@
+package com.snbc.serialportdemo;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+
+import com.snbc.serialportdemo.android_serialport_api.SerialPort;
+import com.snbc.serialportdemo.android_serialport_api.SerialPortFinder;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.InvalidParameterException;
+
+/**
+ * author: zhougaoxiong
+ * date: 2019/7/10,14:34
+ * projectName:SerialPortDemo
+ * packageName:com.snbc.serialportdemo
+ */
+public class SerialPortApplication extends Application {
+
+    public SerialPortFinder mSerialPortFinder = new SerialPortFinder();
+    private SerialPort mSerialPort = null;
+
+    public SerialPort getSerialPort() throws SecurityException, IOException, InvalidParameterException {
+        if (mSerialPort == null) {
+            /* Read serial port parameters */
+            SharedPreferences sp = getSharedPreferences("android_serialport_api.sample_preferences", MODE_PRIVATE);
+            String path = sp.getString("DEVICE", "");
+            int baudrate = Integer.decode(sp.getString("BAUDRATE", "-1"));
+
+            /* Check parameters */
+            if ((path.length() == 0) || (baudrate == -1)) {
+                throw new InvalidParameterException();
+            }
+
+            /* Open the serial port */
+            mSerialPort = new SerialPort(new File(path), baudrate, 0);
+        }
+        return mSerialPort;
+    }
+
+    public void closeSerialPort() {
+        if (mSerialPort != null) {
+            mSerialPort.close();
+            mSerialPort = null;
+        }
+    }
+
+}
